666187bc21effaceb39b578d2b65f3a118f3ffc2
Shreya Sharma <shreya.sharma@rbhsolutions.com>
2026-08-18 16:40:37 +0530

    Env set to local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   productName: 'RsS',
   productVersion: '6.0.1',
   // Set this to your backend IP on the same local network, e.g. 'http://192.168.1.100:5000'
-  // baseURL: 'http://10.0.2.2:5000', // To run the localhost on emulator
+  baseURL: 'http://10.0.2.2:5000', // To run the localhost on emulator
   // baseURL: 'http://192.168.137.1:5000' //To check the local deploy on device
   // baseURL: 'https://164.52.199.179:8001', //backend server,.
   //baseURL: 'http://164.52.199.179:5984', //backend server
